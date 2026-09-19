26dab6b8ea364a615df6da5ceb7255d9a24f58c9
Hamid Karimi <hameedukarimi@gmail.com>
2026-09-19 16:14:05 +0430

    feat: About page create

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ import WhyChooseUsPage from "./admin/pages/why-choose-us/WhyChooseUsPage";
 
 import ServiceDetailsPage from "./pages/ServiceDetailsPage";
 import ContactPage from "./pages/ContactPage"; // <-- 1. Import ContactPage
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<ContactPage />} /> {/* <-- 2. Add Route */}
+          <Route path="/about" element={<AboutPage />} />
 
           <Route path="/admin/login" element={<AdminLogin />} />
 
